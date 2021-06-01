743985ee097e2f13a1d8ec5ce55bf0d8bfac9ee1
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-06-01 11:13:05 +0300

    ASU-713: javascript for application additional applicant

--- public/modules/custom/asu_application/assets/js/additional-applicant.js
+++ public/modules/custom/asu_application/assets/js/additional-applicant.js
@@ -1,3 +1,15 @@
 (function ($) {
-  console.log('asdasd');
+  const applicantWrapper = $('#applicant-wrapper');
+  const button = $('#has-additional-applicant');
+
+  applicantWrapper.hide();
+
+  button.click(function(){
+    if((button.prop('checked'))){
+      applicantWrapper.show();
+    }
+    else {
+      applicantWrapper.hide();
+    }
+  });
 }(jQuery))
