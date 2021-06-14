f82a4006a6cf1399c96c2a3ef0d802c350aa8f9f
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-06-14 13:22:56 +0300

    ASU-662: code fixes

--- public/modules/custom/asu_api/src/Api/Response.php
+++ public/modules/custom/asu_api/src/Api/Response.php
@@ -9,13 +9,27 @@ use Psr\Http\Message\ResponseInterface;
  */
 abstract class Response {
 
-  abstract static function createFromHttpResponse(ResponseInterface $response): Response;
+  /**
+   * Create response class from http client's response.
+   */
+  abstract public static function createFromHttpResponse(ResponseInterface $response): Response;
 
+  /**
+   * Check if request returns 2xx response.
+   *
+   * @param \Psr\Http\Message\ResponseInterface $response
+   *   Guzzle http response.
+   *
+   * @return bool
+   *   Is request 2xx.
+   *
+   * @throws \Exception
+   */
   public static function requestOk(ResponseInterface $response): bool {
     if ($response->getStatusCode() < 200 && $response->getStatusCode() > 299) {
       throw new \Exception('Bad status code: ' . $response->getStatusCode());
     }
-    return true;
+    return TRUE;
   }
 
 }
