68f6aec348daa4d7c7b5feb414bd78415ed31b6c
shamalainen <sebastian@hamse.fi>
2021-06-15 13:01:31 +0300

    ASU-865: Add access to create applications to anomuumi and authenticated users

---
+++ public/modules/custom/asu_application/src/Entity/Access/ApplicationEntityAccess.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\asu_application\Entity\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Defines the access control handler for remote entities.
+ */
+class ApplicationEntityAccess extends EntityAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+
+    switch ($operation) {
+      case 'view':
+        return AccessResult::allowedIfHasPermission($account, 'view remote entities');
+
+      case 'update':
+        return AccessResult::allowedIfHasPermissions($account, [
+          'edit remote entities',
+          'administer remote entities',
+        ], 'OR');
+    }
+
+    return AccessResult::neutral();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+    return AccessResult::allowedIfHasPermissions($account, [
+      'create application'
+    ]);
+  }
+
+}
