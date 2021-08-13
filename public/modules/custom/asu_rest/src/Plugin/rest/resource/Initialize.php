7bfb7e38c9ea53cfffffb43c83bb66c3444ce486
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-08-13 14:53:20 +0300

    prevent unauthenticated user from receiving applications.

--- public/modules/custom/asu_rest/src/Plugin/rest/resource/Initialize.php
+++ public/modules/custom/asu_rest/src/Plugin/rest/resource/Initialize.php
@@ -45,8 +45,16 @@ final class Initialize extends ResourceBase {
     $response['apartment_application_status'] = $this->getApartmentApplicationStatus();
     $response['token'] = \Drupal::service('csrf_token')->get(CsrfRequestHeaderAccessCheck::TOKEN_KEY);
 
+    $response['user'] = [
+      'user_id' => 0,
+      'email_address' => '',
+      'username' => '',
+      'applications' => []
+    ];
+
     /** @var \Drupal\user\Entity\User $user */
-    if ($user = User::load(\Drupal::currentUser()->id())) {
+    if (User::load(\Drupal::currentUser())->isAuthenticated()) {
+      $user = User::load(\Drupal::currentUser()->id());
       $response['user'] = $this->getUser($user);
       $response['user']['applications'] = $this->getUserApplications($user);
     }
